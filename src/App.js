77714c7f3e28c922703e3bfca515118b456f0bdb
marykim0115 <marykim0115@gmail.com>
2024-03-02 20:18:53 +0900

    clock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./css/App.css";
 import Login from "./js/Login";
+import Clock from "./js/Clock";
 
 function App() {
   return (
     <div>
+      <Clock />
       <Login />
     </div>
   );
--- src/js/Login.js
+++ src/js/Login.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "../css/common.css";
 
 const Login = () => {
   const [username, setUsername] = useState("");
